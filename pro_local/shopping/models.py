6371d10377a7dc403fdd23255965460f8603b13c
Matthew S Giancola <matthew.giancola42@gmail.com>
2021-04-22 11:39:16 -0400

    URLs setup

--- pro_local/shopping/models.py
+++ pro_local/shopping/models.py
@@ -2,35 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-# class Shop(models.Model):
-#     s_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
-#     p_FK = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     address = models.CharField(max_length=120)
-#     city = models.CharField(max_length=120)
-#     province = models.CharField(max_length=120)
-#     postal_code = models.CharField(max_length=6)
+ class Shop(models.Model):
+     s_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
+     p_FK = models.ForeignKey(Product, on_delete=models.CASCADE)
+     address = models.CharField(max_length=120)
+     city = models.CharField(max_length=120)
+     province = models.CharField(max_length=120)
+     postal_code = models.CharField(max_length=6)
 
 #     def __str__(self):
 # 		    return self.w_name
 
-# class Product(models.Model):
-#     category_choices = (
-#         ('Men', 'Men'),
-#         ('Women', 'Women'),
-#         ('Kids', 'Kids')
-#     )
-#     type_choices = (
-#         ('Shoes', 'Shoes'),
-#         ('T-shirts', 'T-shirts'),
-#         ('Pants', 'Pants'),
-#         ('Jackets', 'Jackets')
-#     )
-#     p_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
-#     price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=9)
-#     s_FK = models.ForeignKey(Shop, on_delete=models.CASCADE)
-#     p_category = models.TextField(choices=category_choices, null=False, blank=False)
-#     p_type = models.TextField(choices=type_choices, null=False, blank=False)
-#     img = models.ImageField(blank=null, null=false)
+ class Product(models.Model):
+
+     p_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
+     p_keys = models.CharField(null=False, blank=False, max_length=160, unique=True)
+     price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=9)
+     s_FK = models.ForeignKey(Shop, on_delete=models.CASCADE)
+     img = models.ImageField(blank=null, null=false)
     #pip install Pillow for the images to work
 
     # https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=H4N+3B6&destinations=H4N+3K1&key=AIzaSyATsUAiN8HGEmtdItkO3n5E74FEKAelw5o
--- pro_local/shopping/views.py
+++ pro_local/shopping/views.py
@@ -5,11 +5,47 @@ import json
 # Create your views here.
 
 
-def index(request):
+def mapsAPITest(request):
     google_map_api_key = "AIzaSyATsUAiN8HGEmtdItkO3n5E74FEKAelw5o"
-    origin = "H4N 3B6" #user postal code
-    destination = "H4N 3K1" #Store postal code, taken from the shops model
-    res = urllib.request.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=H4N+3B6&destinations=H4N+3K1&key=AIzaSyATsUAiN8HGEmtdItkO3n5E74FEKAelw5o").read()
+    origin = "H4N+3B6" #user postal code
+    destination = "H4N+3K1" #Store postal code, taken from the shops model
+    res = urllib.request.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins="+origin+"&destinations="+destination+"&key=AIzaSyATsUAiN8HGEmtdItkO3n5E74FEKAelw5o").read()
     data = json.loads(res.decode())
     #print(data["rows"][0]["elements"][0]["distance"])
     return HttpResponse(data["rows"][0]["elements"][0]["distance"]["text"])
+
+def cart(request):
+    # this takes care of generating all the information for the inventory view.
+    context = {}
+    if request.method == 'GET':
+        context = {
+
+        }
+    return render(request, 'cart.html', context=context) # render the view
+
+def about(request):
+    # this takes care of generating all the information for the inventory view.
+    context = {}
+    if request.method == 'GET':
+        context = {
+
+        }
+    return render(request, 'about.html', context=context) # render the view
+
+def shop(request):
+    # this takes care of generating all the information for the inventory view.
+    context = {}
+    if request.method == 'GET':
+        context = {
+
+        }
+    return render(request, 'shop.html', context=context) # render the view
+
+def stores(request):
+    # this takes care of generating all the information for the inventory view.
+    context = {}
+    if request.method == 'GET':
+        context = {
+
+        }
+    return render(request, 'stores.html', context=context) # render the view
