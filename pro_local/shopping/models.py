ff197ce0b36cffe2602cd1dd3e166027c5a4deef
AbdulSir <abd.sirawan@hotmail.com>
2021-04-22 13:05:50 -0400

    Configured the models and registered them in admin

--- pro_local/shopping/models.py
+++ pro_local/shopping/models.py
@@ -2,25 +2,26 @@ from django.db import models
 
 # Create your models here.
 
- class Shop(models.Model):
-     s_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
-     #p_FK = models.ForeignKey(Product, on_delete=models.CASCADE)
-     address = models.CharField(max_length=120)
-     city = models.CharField(max_length=120)
-     province = models.CharField(max_length=120)
-     blurb = models.CharField(max_length=1000)
-     postal_code = models.CharField(max_length=6)
+class Shop(models.Model):
+    s_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
+    address = models.CharField(max_length=120)
+    city = models.CharField(max_length=120)
+    province = models.CharField(max_length=120)
+    blurb = models.CharField(max_length=1000)
+    postal_code = models.CharField(max_length=6)
 
-#     def __str__(self):
-# 		    return self.w_name
+    def __str__(self):
+        return self.s_name
 
- class Product(models.Model):
+class Product(models.Model):
+    p_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
+    p_keys = models.CharField(null=False, blank=False, max_length=160, unique=True)
+    price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=9)
+    s_FK = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    img = models.ImageField(blank=False, null=False)
 
-     p_name = models.CharField(null=False, blank=False, max_length=80, unique=True)
-     p_keys = models.CharField(null=False, blank=False, max_length=160, unique=True)
-     price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=9)
-     s_FK = models.ForeignKey(Shop, on_delete=models.CASCADE)
-     img = models.ImageField(blank=null, null=false)
+    def __str__(self):
+        return self.p_name
     #pip install Pillow for the images to work
 
     # https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=H4N+3B6&destinations=H4N+3K1&key=AIzaSyATsUAiN8HGEmtdItkO3n5E74FEKAelw5o
