1107f46dd37c4c34629d6ff5f62169263cd572db
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2021-04-23 10:42:38 -0400

    Import Error Fix #2

--- pro_local/shopping/urls.py
+++ pro_local/shopping/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     path('', views.shop, name='shop'),
     path('about/', views.about, name='about'),
     path('cart/', views.cart, name='cart'),
-    path('404/', views.404),
-    path('500/', views.500),
 ]
 
-handler404 = views.404
-handler500 = views.500
 
