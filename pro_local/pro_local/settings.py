57cf3a2d3672d0e54bf31d199a341e5c0fe25a27
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2021-04-23 09:03:42 -0400

    Fix for error H10 #2

--- pro_local/pro_local/settings.py
+++ pro_local/pro_local/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopping',
+    'pro_local.shopping',
 ]
 
 MIDDLEWARE = [
