d5528cd236ebce84a6d9e35db42af255324dffc4
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2021-04-23 09:18:44 -0400

    Fix for allowed host error #4

--- pro_local/pro_local/urls.py
+++ pro_local/pro_local/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('shopping.urls')),
+    path('', include('pro_local.shopping.urls')),
     path('admin/', admin.site.urls),
 ]
